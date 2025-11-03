1e4925e3262ed8469252f65c7faa8b0d7e17266c
maxmakovskiy <makovskiymax@gmail.com>
2025-11-03 12:05:10 +0100

    config: adding inital config

---
+++ src/test/java/ch/heigvd/dai/retrivium/MainTest.java
@@ -0,0 +1,12 @@
+package ch.heigvd.dai.retrivium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    public void testTest() {
+        assertTrue(1 > 0);
+    }
+}
