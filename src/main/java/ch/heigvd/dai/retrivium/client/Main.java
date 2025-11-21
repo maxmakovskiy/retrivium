49f389365cbdec47515668084614470e05fe0316
maxmakovskiy <makovskiymax@gmail.com>
2025-11-21 12:14:10 +0100

    feat: removing unnecessary Main-s

--- src/main/java/ch/heigvd/dai/retrivium/client/Main.java
+++
@@ -1,8 +0,0 @@
-package ch.heigvd.dai.retrivium.client;
-
-public class Main {
-    public static void main(String[] args) {
-        TcpReplClient client = new TcpReplClient("localhost", 1234, '\n');
-        client.launch();
-    }
-}
--- src/main/java/ch/heigvd/dai/retrivium/server/Main.java
+++
@@ -1,8 +0,0 @@
-package ch.heigvd.dai.retrivium.server;
-
-public class Main {
-    public static void main(String[] args) {
-        TcpServer server = new TcpServer(1234, '\n');
-        server.launch();
-    }
-}
