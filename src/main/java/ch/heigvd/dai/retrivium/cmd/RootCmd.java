de610c51f24d98a1538f32e521b6ae15ef63c8b7
AlterSpectre <guoyu.wu.01@gmail.com>
2025-12-02 16:58:26 +0100

    javadoc on rootCmd

--- src/main/java/ch/heigvd/dai/retrivium/cmd/RootCmd.java
+++ src/main/java/ch/heigvd/dai/retrivium/cmd/RootCmd.java
@@ -2,6 +2,9 @@ package ch.heigvd.dai.retrivium.cmd;
 
 import picocli.CommandLine;
 
+/**
+ * Define the existing commands from the default cmd wish display either the client side or the server side
+ */
 @CommandLine.Command(
         description = "A small TCP-based search engine",
         version = "1.0.0",
